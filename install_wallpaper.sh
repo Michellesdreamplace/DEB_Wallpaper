567aeaff8a26ce15126aab385505ad2d0b00a98a
Michelle <172150370+Michellesdreamplace@users.noreply.github.com>
2024-08-28 12:42:00 +0200

    update

--- install_wallpaper.sh
+++ install_wallpaper.sh
@@ -1,42 +1,42 @@
 #!/bin/bash
 echo "************************************************************"
-echo "    Update package database (repository index) ..."
+echo "    aktualisiere Paketdatenbank (Repository-Index) ..."
 echo " ..."
 sudo apt update
 echo "************************************************************"
-echo "    Upgrade ..."
+echo "    führe upgrade duch ..."
 echo " ..."
 sudo apt upgrade -y
 echo "************************************************************"
-echo "    Clean system of useless packages ..."
+echo "    bereinige System von nutzlosen Paketen ..."
 echo " ..."
 sudo apt autoremove -y
 echo "************************************************************"
-echo "    install git ..."
+echo "    installiere git ..."
 echo " ..."
 sudo apt install git -y
 echo "************************************************************"
-echo "    install tar ..."
+echo "    installiere tar ..."
 echo " ..."
 sudo apt install tar -y
 echo "************************************************************"
-echo "    clone Wallpaper from github ..."
+echo "    klone Hintergrundbilder von GitHub ..."
 echo " ..."
 sudo git clone https://github.com/Michellesdreamplace/DEB_Wallpaper.git ~/Downloads/MDP_DEB_Wallpaper/
 echo "************************************************************"
-echo "    unpack Wallpaper into global share folder ..."
+echo "    entpacke Hintergrundbilder in globalen Freigabeordner ..."
 echo " ..."
 sudo tar -vxf ~/Downloads/MDP_DEB_Wallpaper/MDP_Wallpaper.tar -C /usr/share/images/desktop-base/
 echo "************************************************************"
-echo "    clean up ..."
+echo "    aufräumen ..."
 echo " ..."
 sudo rm -r ~/Downloads/MDP_DEB_Wallpaper
 echo "************************************************************"
 echo " "
-echo "    Done ..."
+echo "    Fertig ..."
 echo " "
-echo "  Now you can find the new wallpapers in the global directory at:"
+echo "  Du findst die neuen Hintergrundbilder im globalen Verzeichnis unter:"
 echo "  /usr/share/images/desktop-base/"
-echo "  Have fun with it :)"
+echo "  Viel Spaß damit :)"
 echo " "
 echo "************************************************************"
