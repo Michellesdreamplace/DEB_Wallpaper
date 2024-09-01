175be7db205174d112c961f7948eea09f9c3518e
Michelle <172150370+Michellesdreamplace@users.noreply.github.com>
2024-09-01 20:10:17 +0200

    update

--- install_wallpaper.sh
+++ install_wallpaper.sh
@@ -22,6 +22,7 @@ sudo git clone https://github.com/Michellesdreamplace/DEB_Wallpaper.git $HOMEVER
 echo "************************************************************"
 echo "    entpacke Hintergrundbilder in globalen Freigabeordner ..."
 echo " ..."
+sudo mkdir -p /usr/share/images/desktop-base/MDP-Wallpaper
 sudo tar -xzf $HOMEVERZEICHNIS/Downloads/MDP_DEB_Wallpaper/MDP_Wallpaper.tar.gz -C /usr/share/images/desktop-base/MDP-Wallpaper/
 echo "************************************************************"
 echo "    aufräumen ..."
